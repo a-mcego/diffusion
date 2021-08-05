ce4f6ffee6cad02cac1a0a04322b82c92db16c02
Angela McEgo <eve.loikkanen@gmail.com>
2021-08-05 21:07:23 +0300

    U-Net and it works now
    Additions and fixes:
    
    1) The network is now a residual U-Net, removed the previous convnet
    2) Removed unused parameters from prm
    3) The sampling process is changed completely.
    
    Other stuff:
    
    1) Removed output pictures that were added accidentally.

--- mnist.py
+++ mnist.py
@@ -50,29 +50,33 @@ def get_pngs_from_directory(directory):
     return imgs
 
 #dataset = get_mnist_pics_only()
+#dataset = tf.pad(dataset, [[0,0],[2,2],[2,2],[0,0]], constant_values = -1.0) #pad to 32x32
+
 dataset = get_pngs_from_directory("Q:\\doom\\out")
-dataset_stddev = tf.math.reduce_std(dataset)
-dataset_mean = tf.math.reduce_mean(dataset)
+dataset = tf.pad(dataset,[[0,0],[0,8],[0,0],[0,0]],constant_values=-1.0) #pad to 128x80
+
+dataset_stddev = tf.math.reduce_std(dataset,axis=[0,1,2],keepdims=True)
+dataset_mean = tf.math.reduce_mean(dataset,axis=[0,1,2],keepdims=True)
 dataset = (dataset-dataset_mean)/dataset_stddev
 
+print(f"Dataset shape: {dataset.shape}")
+
 prm = {} #parameters
-prm['N_STEPS'] = 99 #how many diffusion steps we use
+prm['N_STEPS'] = 199 #how many diffusion steps we use
 prm['SIZE_Y'] = dataset.shape[-3]
 prm['SIZE_X'] = dataset.shape[-2]
 prm['CHANNELS'] = dataset.shape[-1]
 prm['IMAGE_SIZE'] = prm['SIZE_Y']*prm['SIZE_X']*prm['CHANNELS']
-prm['LAYERS'] = 6
-prm['MODEL_SIZE'] = 128
 prm['BATCH_SIZE'] = 32
 prm['LEARNING_RATE_START'] = 0.001
 prm['LEARNING_RATE_MIN'] = 0.0001
 prm['ADAM_EPSILON'] = 1e-4
-prm['N_TEST_GENERATIONS'] = 16
-prm['FILTER_SIZE'] = 3
+prm['N_TEST_GENERATIONS'] = 9
 prm['USE_RANGE_SHUFFLER'] = True
 prm['PRINT_TIME'] = 32 #how many steps between prints
 prm['GENERATE_TIME'] = prm['PRINT_TIME']*8 # how many steps between generated outputs saved to .png
-prm['USE_TIMESTEP_EMBEDDINGS'] = False
+prm['USE_TIMESTEP_EMBEDDINGS'] = True
+prm['USE_IMAGE_POSENC'] = False
 
 for key in prm:
     print(f"{key}={prm[key]} ",end="")
@@ -106,7 +110,7 @@ class NoisingProcess:
 def reduce(matrix, axis):
     mean = tf.math.reduce_mean(matrix, axis=axis, keepdims=True)
     stddev = tf.math.reduce_std(matrix, axis=axis, keepdims=True)
-    return (matrix - mean)/(stddev + 1e-2)
+    return (matrix - mean)/(stddev + 1e-3)
 
 #pad image *amount* amount on all sides, up/down and left/right, such that it repeats
 def pad_image_repeat(img, amount):
@@ -126,20 +130,57 @@ def normalize_features(x):
     x = tf.reshape(x,oldshape)
     return x
 
-class NoisePredictorConv(tf.keras.Model):
-    def __init__(self):
-        super(NoisePredictorConv,self).__init__()
-        self.timestep_posenc = posenc.get_zero_to_one_posenc(prm['N_STEPS'], prm['MODEL_SIZE'])
+#the resolution-preseving parts of U-NET
+class MultiConv(tf.keras.Model):
+    def __init__(self, model_size):
+        super(MultiConv,self).__init__()
         
-        self.image_posenc = tf.keras.layers.Embedding(prm['SIZE_X']*prm['SIZE_Y'], prm['MODEL_SIZE'])
+        self.filter_size = 3
+        
+        self.convs = [tf.keras.layers.Conv2D(model_size, self.filter_size, activation=tf.nn.gelu, padding='valid') for _ in range(2)]
+        
+    def call(self, data):
+        output = data
+        for layer in self.convs:
+            orig = output
+            output = pad_image_repeat(output, self.filter_size//2)
+            output = orig+layer(output)
+            #output = layer(output)
+            output = normalize_features(output)
+        return output
+        
 
-        self.prologue = tf.keras.layers.Conv2D(prm['MODEL_SIZE'], (1,1), use_bias=False)
-        self.convs = [tf.keras.layers.Conv2D(prm['MODEL_SIZE'], (prm['FILTER_SIZE'],prm['FILTER_SIZE']), activation=tf.nn.gelu, padding='valid') for _ in range(prm['LAYERS'])]
+class NoisePredictorUNet(tf.keras.Model):
+    def __init__(self):
+        super(NoisePredictorUNet,self).__init__()
+
+        #layer amount hardcoded for now
+        self.features = [64,128,256,512,1024,512,256,128,64]
+        self.concat   = [-1, -1, -1, -1,  -1,  3,  2,  1, 0]
+        
+        self.n_layers = len(self.features)
+        
+        downsamples = [tf.keras.layers.Conv2D(self.features[n+1], (2,2), strides=(2,2), activation=tf.nn.gelu) for n in range(self.n_layers//2)]
+        upsamples = [tf.keras.layers.Conv2DTranspose(self.features[self.n_layers//2+1+n], (1,1), strides=(2,2), activation=tf.nn.gelu) for n in range(self.n_layers//2)]
+        
+        self.samples = []
+        self.samples.extend(downsamples)
+        self.samples.extend(upsamples)
+        self.samples.extend([None])
+
+        if prm['USE_TIMESTEP_EMBEDDINGS']:
+            self.timestep_posenc = posenc.get_zero_to_one_posenc(prm['N_STEPS'], self.features[0])
+        
+        self.image_posenc = tf.keras.layers.Embedding(prm['SIZE_X']*prm['SIZE_Y'], self.features[0])
+        self.prologue = tf.keras.layers.Conv2D(self.features[0], (1,1), use_bias=False)
+        self.convs = [MultiConv(self.features[n]) for n in range(self.n_layers)]
         self.epilogue = tf.keras.layers.Conv2D(prm['CHANNELS'], (1,1), use_bias=False)
 
     def call(self, data, timestep_ns):
         #data shape:        BATCH, SIZEY, SIZEX, CHANNELS
         #timestep_ns shape: BATCH
+        
+        orig_input = data
         
         orig_shape = data.shape
         
@@ -156,23 +197,27 @@ class NoisePredictorConv(tf.keras.Model):
 
         image_posenc = self.image_posenc(tf.range(prm['SIZE_X']*prm['SIZE_Y'],dtype=tf.int64))
         #image_posenc shape: 784, 256
-        image_posenc = tf.reshape(image_posenc, [1,prm['SIZE_Y'],prm['SIZE_X'],prm['MODEL_SIZE']])
+        image_posenc = tf.reshape(image_posenc, [1,prm['SIZE_Y'],prm['SIZE_X'],self.features[0]])
         #image_posenc shape: 1, 28, 28, 256
         #20.0 is an arbitrary constant to make it actually see the posenc.
         #i want to normalize these things somehow at some point.
         output += image_posenc*20.0
-
-        output = normalize_features(output)
-
-        for layer in self.convs:
-            output = pad_image_repeat(output, prm['FILTER_SIZE']//2)
-            output = layer(output)
-            output = normalize_features(output)
-        output = self.epilogue(output)
         
+        #now we do the actual network
+        outputs = []
+        for n in range(self.n_layers):
+            if self.concat[n] != -1:
+                output += outputs[self.concat[n]] #can't concat because of residual network, so we just add.
+            output = self.convs[n](output)
+            outputs.append(output)
+            
+            if self.samples[n] is not None:
+                output = self.samples[n](output)
+            
+        output = self.epilogue(output)
         output = normalize_features(output)
         return output
-        
+
 step=1
 
 def lr_scheduler():
@@ -183,7 +228,7 @@ def lr_scheduler():
 
 optimizer = tf.keras.optimizers.Adam(learning_rate=lr_scheduler,amsgrad=True,epsilon=prm['ADAM_EPSILON'])
 noising_process = NoisingProcess(prm['N_STEPS'])
-noise_predictor = NoisePredictorConv()
+noise_predictor = NoisePredictorUNet()
 
 @tf.function
 def do_step(inputdata, timesteps, target):
@@ -268,18 +313,14 @@ while True:
             picture = tf.random.normal(shape=[n_gen,dataset.shape[1],dataset.shape[2],dataset.shape[3]], stddev=1.0, mean=0.0)
             for ts in range(prm['N_STEPS'], 0, -1):
                 ts_tf = tf.expand_dims(tf.convert_to_tensor(ts),axis=0)
-                ts_tf = tf.broadcast_to(ts_tf,[n_gen])
-                picture -= noise_predictor(picture, ts_tf)*noising_process.get_noise_stddev_for_step(ts)
+                ts_tf = tf.broadcast_to(ts_tf,[picture.shape[0]])
                 
-                pictureshape = picture.shape
-                picture = tf.reshape(picture,[picture.shape[0],-1])
-                picture = reduce(picture, axis=-1)
-                picture = tf.reshape(picture,pictureshape)
+                oldnoise = noising_process.get_noise_stddev_for_step(ts)
+                newnoise = noising_process.get_noise_stddev_for_step(ts-1)
                 
-                picture *= noising_process.get_image_stddev_for_step(ts-1)
+                denoised_picture = picture-noise_predictor(picture, ts_tf)*oldnoise
+                picture = denoised_picture + tf.random.normal(shape=picture.shape, mean=0, stddev=newnoise)
                 
-                picture,_ = noising_process.direct(ts-1, picture)
-            
             picture = picture*dataset_stddev+dataset_mean
             picture = gridify(picture)
             picture = tf.cast(tf.clip_by_value(((picture+1.0)*0.5)*255.0,0.0,255.0),dtype=tf.uint8)
